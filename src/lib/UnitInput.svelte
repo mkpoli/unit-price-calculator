b1cdb7acac90fbfaabcb8834f91744d09f4d05bc
mkpoli <snowwolf1001@gmail.com>
2021-04-27 23:42:25 +0900

    Add math expression evaluation

--- src/lib/UnitInput.svelte
+++ src/lib/UnitInput.svelte
@@ -1,10 +1,23 @@
 <script lang="ts">
+  import mexp from 'math-expression-evaluator';
+
   export let value: number
   export let unit: string
+  let realValue: string = `${value}`
+
+  function onInput() {
+    try {
+      const result = mexp.eval(realValue)
+      value = result 
+    } catch {
+      return
+    }
+  }
+
 </script>
 
 <label>
-  <input type="text" bind:value={value} /><span>{ unit }</span>
+  <input type="text" bind:value={realValue} on:input={onInput} title={`${value}`} /><span>{ unit }</span>
 </label>
 
 <style>
