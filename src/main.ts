f3567db5096a33b0e12e3d3d3216856fa92500d2
mkpoli <snowwolf1001@gmail.com>
2021-04-27 22:13:04 +0900

    Install sanitize.css

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,7 @@
 import App from './App.svelte'
+import 'sanitize.css';
+import 'sanitize.css/forms.css';
+import 'sanitize.css/typography.css';
 
 const app = new App({
   target: document.getElementById('app')
