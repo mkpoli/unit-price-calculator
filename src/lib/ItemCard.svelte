baee7d30528166498279d3fa584c33995f6c250a
mkpoli <snowwolf1001@gmail.com>
2021-04-27 22:12:53 +0900

    Style delete button

--- src/lib/ItemCard.svelte
+++ src/lib/ItemCard.svelte
@@ -22,8 +22,10 @@
   <button on:click={() => {dispatch('delete')}}><SvgIcon type="mdi" path={mdiDeleteOutline}/></button>
 </div>
 
-<style>
+<style style lang="postcss">
   .card {
+    position: relative;
+
     width: var(--card-width);
     height: var(--card-height);
 
@@ -36,5 +38,27 @@
     flex-direction: column;
     gap: 0.5rem;
     text-align: center;
+  }
+
+  button {
+    position: absolute;
+    top: 1rem;
+    right: 1rem;
+
+    padding: 0;
+
+    appearance: none;
+    background: none;
+    border: none;
+    color: #aaa;
+
+    &:hover {
+      color: #000
+    }
+
+    &:focus {
+      outline: none;
+      color: #ff0032;
+    }
   }
 </style>
